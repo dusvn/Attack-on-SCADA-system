b535b0957950542bc4b6767cd9a9517b90441fa0
Dusan Paripovic <paripovicdusanftn@gmail.com>
2023-10-22 20:28:33 +0200

    izmenjeno za htons

--- SCADA-HMI/BEZBEDNOST_SCADASTANICA/SCADA-HMI/ReadRequest.py
+++ SCADA-HMI/BEZBEDNOST_SCADASTANICA/SCADA-HMI/ReadRequest.py
@@ -27,30 +27,14 @@ class ModbusReadRequest(ModbusBase):
     
 def repack(ReadRequest: ModbusReadRequest) -> bytearray:
     request = bytearray(12)
-    
-    transaction_id = struct.pack('>H', ReadRequest.TransactionID)
-    request[0:2] = transaction_id
-    print(request[0:2])
+    request[0:2] = socket.htons(ReadRequest.TransactionID)
+    request[2:4] = socket.htons(ReadRequest.ProtocolID)
+    request[4:6] = socket.htons(ReadRequest.Length)
+    request[6] = ReadRequest.UnitID
+    request[7] = ReadRequest.FunctionCode
+    request[8:10] =  socket.htons(ReadRequest.StartAddress)
+    request[10:12] = socket.htons(ReadRequest.Quantity)
 
-    protocol_id = struct.pack('>H', ReadRequest.ProtocolID)
-    request[2:4] = protocol_id
-
-    length = struct.pack('>H', ReadRequest.Length)
-    request[4:6] = length
-
-    unit_id = struct.pack('B', ReadRequest.UnitID)
-    request[6] = unit_id[0]
-
-    function_code = struct.pack('B', ReadRequest.FunctionCode)
-    request[7] = function_code[0]
-
-    start_address = struct.pack('>H', ReadRequest.StartAddress)
-    request[8:10] = start_address
-
-    quantity = struct.pack('>H', ReadRequest.Quantity)
-    request[10:12] = quantity
-
- 
     return request
 
     
